622eaf41582ef797555b5e5e313dd55291b8ee8b
2JCycle <74063123+2JCycle@users.noreply.github.com>
2020-11-06 16:49:34 -0800

    Create Footer.js

---
+++ src/component/Footer.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react'
+import Pic2 from '../img/Group 421.png'
+
+
+class Footer extends Component{
+    render(){
+        return(
+            <div>
+                <img src={Pic2} alt="deedee" id="kay"/>     
+
+            </div>
+        )
+    }
+}
+
+export default Footer
